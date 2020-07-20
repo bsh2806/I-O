c3678733e6c34d17dca6a522b59db6a3db84af84
kihong.In <kihong.In@DESKTOP-K5M28F0>
2020-07-20 10:51:52 +0900

    I/O

---
+++ src/com/javalec/ex/MainClass.java
@@ -0,0 +1,36 @@
+package com.javalec.ex;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainClass {
+	public static void main(String[] args) {
+		InputStream is = null;
+		OutputStream os = null;
+		
+		try {
+			is = new FileInputStream("C:\\Users\\kihong.In\\Downloads\\jain.txt");
+			os = new FileOutputStream("C:\\Users\\kihong.In\\Downloads\\jain_copy.txt");
+			
+			while (true) {
+				int i = is.read();
+				if (i == -1) break;
+				os.write(i);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if (is != null) is.close();
+				if (os != null) os.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+				System.out.println(e2.getMessage());
+			}
+		}
+	}
+} 
+ 
